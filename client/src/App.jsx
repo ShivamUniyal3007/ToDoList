bbc99177086f34790d972a7b05c9fcec4a74e191
ShivamUniyal3007 <Shivam.sharma30072001@gmail.com>
2024-05-28 16:13:48 +0530

    Make home private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,16 @@ import Home from "./pages/Home";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
     <Header/>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+      <Route element={<PrivateRoute/>}>
+          <Route path='/' element={<Home/>} ></Route>
+          </Route>
         <Route path="/sign-in" element={<Signin />}></Route>
         <Route path="/sign-up" element={<Signup />}></Route>
       </Routes>
